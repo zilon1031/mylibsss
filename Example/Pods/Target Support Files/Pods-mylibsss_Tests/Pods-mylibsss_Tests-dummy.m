3806b27b2cd78b6dcad58a850d7e6bcf4e39bca4
zilon <zilon1031@sina.com>
2019-04-11 21:39:06 +0800

    init library

---
+++ Example/Pods/Target Support Files/Pods-mylibsss_Tests/Pods-mylibsss_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mylibsss_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_mylibsss_Tests
+@end
---
+++ Example/Pods/Target Support Files/mylibsss/mylibsss-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_mylibsss : NSObject
+@end
+@implementation PodsDummy_mylibsss
+@end
